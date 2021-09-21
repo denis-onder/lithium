3ada351a204e0c13ce1f9ba9285c20bfa0bea133
Denis Onder <denis_onder@protonmail.com>
2021-09-21 21:48:37 +0200

    Basic domains

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct Transaction {
+    from_address: String,
+    to_address: String,
+    amount: i32,
+}
+
+struct Block {
+    hash: String,
+    created_at: i64, // Timestamp
+    data: Transaction,
+    previousBlockHash: String,
+    nonce: i32,
+}
+
+type Blockchain = Vec<Block>;
+
 fn main() {
+    let lithium: Blockchain = Vec::new();
     println!("Hello, world!");
 }
