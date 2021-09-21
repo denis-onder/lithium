dae4f26cdcf004ac5653bcc917c9fce09cbdea33
Denis Onder <denis_onder@protonmail.com>
2021-09-21 22:04:56 +0200

    Adding blocks to the chain

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use chrono::Utc;
+
+#[derive(Debug)]
 struct Transaction {
+    hash: String,
     from_address: String,
     to_address: String,
     amount: i32,
 }
 
+#[derive(Debug)]
 struct Block {
     hash: String,
     created_at: i64, // Timestamp
@@ -14,7 +19,47 @@ struct Block {
 
 type Blockchain = Vec<Block>;
 
+fn create_transaction(from_address: String, to_address: String, amount: i32) -> Transaction {
+    return Transaction {
+        hash: String::from("TEST"),
+        from_address: from_address,
+        to_address: to_address,
+        amount: amount,
+    };
+}
+
+fn create_block(
+    hash: String,
+    created_at: i64,
+    data: Transaction,
+    previousBlockHash: String,
+    nonce: i32,
+) -> Block {
+    return Block {
+        hash: hash,
+        created_at: created_at,
+        data: data,
+        previousBlockHash: previousBlockHash,
+        nonce: nonce,
+    };
+}
+
 fn main() {
-    let lithium: Blockchain = Vec::new();
-    println!("Hello, world!");
+    let date_time = Utc::now();
+    // let difficulty = 5;
+
+    let tx1 = create_transaction(String::from("null"), String::from("null"), 0);
+    let block1 = create_block(
+        String::from("TEST"),
+        date_time.timestamp(),
+        tx1,
+        String::from("HEAD"),
+        0,
+    );
+
+    let mut lithium: Blockchain = Vec::new();
+
+    lithium.push(block1);
+
+    println!("{:?}", lithium);
 }
